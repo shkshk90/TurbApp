614ae960c4b83dcf33eaf47bef5400e91ee45584
shkshk90 <shkshk90@gmail.com>
2019-03-03 14:34:01 +0100

    Initial Commit

---
+++ TurbApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TurbApp
+//
+//  Created by Samuel Aysser on 03.03.19.
+//  Copyright © 2019 Fraunhofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TurbApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TurbApp
+//
+//  Created by Samuel Aysser on 03.03.19.
+//  Copyright © 2019 Fraunhofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
